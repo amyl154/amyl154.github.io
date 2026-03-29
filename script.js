4dc04e251beccf001565ca1b134b03393ef5bad6
amyl154 <165519089+amyl154@users.noreply.github.com>
2026-03-28 22:20:14 -0400

    used js background function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const button = document.getElementById("showBioBtn");
 const extraBio = document.getElementById("extraBio");
-//const bgButton = document.getElementById("bgChangeBtn");
-//const backgrounds = [() => magicpattern(),   // your function that returns a CSS pattern 
-//"green"];
-//let current = 0;
+const bgButton = document.getElementById("bgChangeBtn");
+const backgrounds = [() => magicpattern(),   // your function that returns a CSS pattern 
+() => magicpattern2()];
+
 
 button.addEventListener("click", function() {
     if (extraBio.style.display === "none") {
@@ -15,11 +15,30 @@ button.addEventListener("click", function() {
     }
 });
 
-// list of backgrounds (colors or CSS patterns)
-// bgButton.addEventListener("click", () => {
-// current = (current + 1) % backgrounds.length; // cycle through backgrounds
-// const bg = typeof backgrounds[current] === "function" ? backgrounds[current]() : backgrounds[current];
-// Apply it to the page
-// document.body.style.background = bg; // change background
-// });
+function magicPattern(bgColor, dotColor = "#ffe4e4") {
+  return `url("data:image/svg+xml;utf8,
+  <svg viewBox='0 0 200 140' xmlns='http://www.w3.org/2000/svg'>
+    <rect width='100%' height='100%' fill='${bgColor}'/>
+    <circle cx='20' cy='20' r='5' fill='${dotColor}'/>
+    <circle cx='60' cy='20' r='5' fill='${dotColor}'/>
+    <circle cx='100' cy='20' r='5' fill='${dotColor}'/>
+    <circle cx='140' cy='20' r='5' fill='${dotColor}'/>
+    <circle cx='180' cy='20' r='5' fill='${dotColor}'/>
+    
+    <circle cx='40' cy='60' r='5' fill='${dotColor}'/>
+    <circle cx='80' cy='60' r='5' fill='${dotColor}'/>
+    <circle cx='120' cy='60' r='5' fill='${dotColor}'/>
+    <circle cx='160' cy='60' r='5' fill='${dotColor}'/>
+  </svg>")`;
+}
 
+document.body.style.backgroundImage = magicPattern("#b9d09d");
+
+/*
+let current = 0;
+
+bgButton.addEventListener("click", () => {
+    current = (current + 1) % backgrounds.length;
+    document.body.style.backgroundImage = backgrounds[current]
+});
+*/
