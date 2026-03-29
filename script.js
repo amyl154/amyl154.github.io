1ff9510894d551bc0e62d606b3fb51e3814dc767
amyl154 <165519089+amyl154@users.noreply.github.com>
2026-03-28 21:02:14 -0400

    changing backgrounds, dotted pattern

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const button = document.getElementById("showBioBtn");
 const extraBio = document.getElementById("extraBio");
+const bgButton = document.getElementById("bgChangeBtn");
+const backgrounds = [
+  "repeating-radial-gradient(circle, #fff7f3 0px, #000000 5px, transparent 5px, transparent 25px)" // dotted pattern// 
+  ,"repeating-radial-gradient(circle, #000000 0px, #ffffff 5px, transparent 5px, transparent 25px)"
+];
+let current = 0;
 
 button.addEventListener("click", function() {
     if (extraBio.style.display === "none") {
@@ -10,3 +16,10 @@ button.addEventListener("click", function() {
         button.textContent = "Read More";  // change back
     }
 });
+
+// list of backgrounds (colors or CSS patterns)
+bgButton.addEventListener("click", () => {
+  current = (current + 1) % backgrounds.length; // cycle through backgrounds
+  document.body.style.background = backgrounds[current]; // change background
+});
+
