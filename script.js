fb702e9ed31d3581f1fae3a832894d36dafb4b08
amyl154 <165519089+amyl154@users.noreply.github.com>
2026-08-07 18:15:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ function makeSVG(bg, dot) {
 document.addEventListener('DOMContentLoaded', () => {
  
   const bgButton = document.getElementById("bgChangeBtn");
-  const backgrounds = [makeSVG('#d9e3b7', '#ffdada'), makeSVG('#6d7d7d', '#5f5656')];
+  const backgrounds = [makeSVG('#d9e3b7', '#ffdada'), makeSVG('#361800', '#5E4633')];
   const bgImages    = ['images/button1.png', 'images/button2.png']; 
-  const textColors  = ['#30270a', '#fdf6d4'];                       
+  const textColors  = ['#30270a', '#fdf6d4'];    
+  const textColors1  = ['#544334', '#eee4b4'];                       
   const textShadows = ['2px 5px rgb(175, 180, 140)', '2px 5px #929c94'];
  
   let current = 0; 
@@ -37,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
     h1.style.textShadow = textShadows[i];
 
     document.querySelectorAll('.sidebar-links a').forEach(link => {
-    link.style.color = textColors[i];
+    link.style.color = textColors1[i];
     });
  
     // swap button theme
